7b2fb1715dcad58681610dfeab9ce563d7890092
Shubhamkumar-codes <shubhamkumar13911@gmail.com>
2024-02-27 20:13:39 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
 dotenv.config();
 
 mongoose
@@ -16,8 +18,11 @@ mongoose
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.listen(port, () => {
     console.log(`Server is runing on port ${port}`);
 });
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
             <Link to='/'>
                 <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
                     <span className='text-slate-500'>Shubham</span>
-                    <span className='text-slate-700'>Kumar</span>
+                    <span className='text-slate-700'>Estate</span>
                 </h1>
             </Link> 
 

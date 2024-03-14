88d388ef7a484f424a247356dc25bbe0576a09a2
Shubhamkumar-codes <shubhamkumar13911@gmail.com>
2024-03-14 13:16:55 +0530

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -19,6 +20,8 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(port, () => {
     console.log(`Server is runing on port ${port}`);

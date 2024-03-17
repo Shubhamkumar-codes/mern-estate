e9707fdd163d6ecefa91364cee28eda3d0a3e459
Shubhamkumar-codes <shubhamkumar13911@gmail.com>
2024-03-17 18:12:07 +0530

    add sign out functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -9,6 +9,9 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
 } from "../redux/user/userSlice";
 
 import { useDispatch } from "react-redux";
@@ -105,6 +108,21 @@ export default function Profile() {
     }
   };
 
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false){
+        dispatch(signOutUserFailure(data.message));
+        return;
+      }
+      dispatch(signOutUserSuccess(data));
+    } catch (error) {
+      dispatch(signOutUserFailure(data.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
 
@@ -148,7 +166,7 @@ export default function Profile() {
       </form>
       <div  className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer" >Delete account</span>
-        <span className="text-red-700 cursor-pointer" >Sign out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer" >Sign out</span>
       </div>
       <p className="text-red-700 mt-5" >{error ? error : ''}</p>
       <p className="text-green-700 mt-5">{updateSucess ? 'User is updated successfully!' : ''}</p>
